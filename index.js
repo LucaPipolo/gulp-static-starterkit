85d72b4101d433e96cc07043e254fcc45e0e187d
Luca Pipolo <luca@netzstrategen.com>
2019-02-26 18:01:28 +0100

    Fixed path to dependencies folder was wrong.

--- index.js
+++ index.js
@@ -2,29 +2,31 @@
   gulpfile.js
 */
 
-const requireDir = require('require-dir');
+module.exports = function(gulp) {
+  const requireDir = require('require-dir');
 
-// Require configs and all tasks in gulp/tasks, including sub-folders.
-const config = require('./config');
-const tasks = requireDir('./tasks', {
-  recurse: true,
-});
+  // Require configs and all tasks in gulp/tasks, including sub-folders.
+  const config = require('./config');
+  const tasks = requireDir('./tasks', {
+    recurse: true,
+  });
 
-/**
- * Requires all tasks in gulpfile.js/tasks, including subfolders.
- *
- * @param {*} tasks
- */
-function resolveTasks(tasks) {
-  for (const task in tasks) {
-    if ({}.hasOwnProperty.call(tasks, task)) {
-      const fn = tasks[task];
-      if (typeof fn !== 'function') {
-        resolveTasks(fn);
-        continue;
+  /**
+   * Requires all tasks in gulpfile.js/tasks, including subfolders.
+   *
+   * @param {*} tasks
+   */
+  function resolveTasks(tasks) {
+    for (const task in tasks) {
+      if ({}.hasOwnProperty.call(tasks, task)) {
+        const fn = tasks[task];
+        if (typeof fn !== 'function') {
+          resolveTasks(fn);
+          continue;
+        }
+        fn(config);
       }
-      fn(config);
     }
   }
-}
-resolveTasks(tasks);
+  resolveTasks(tasks);
+};
