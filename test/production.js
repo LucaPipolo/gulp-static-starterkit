a8535697b964a3800eb0770deeab6d882a8c3e04
Luca Pipolo <dev@lucapipolo.com>
2019-06-16 17:22:47 +0200

    Fixed JS/CSS minification production tests were failing in case the hashtag change.

--- test/production.js
+++ test/production.js
@@ -1,12 +1,13 @@
 const chai = require('chai');
 const expect = require('chai').expect;
-const fs = require('chai-fs');
-chai.use(fs);
+const chaifs = require('chai-fs');
+const glob = require("glob");
+chai.use(chaifs);
 
 describe('production', function () {
   // Styles.
   it('CSS file is minified.', function(done) {
-    expect('test-website/dist/styles/main-ccdfbef0d1.min.css').to.be.a.file().and.have.contents.that.match(/\*\/(?!\n).*$/);
+    checkMinification('styles', /main-[a-z0-9]{10}\.min\.css/);
     done();
   });
 
@@ -17,7 +18,7 @@ describe('production', function () {
 
   // Scripts.
   it('JavaScript file is minified.', function(done) {
-    expect('test-website/dist/scripts/main-fa27bd3f54.min.js').to.be.a.file().and.have.contents.that.match(/\*\/(?!\n).*$/);
+    checkMinification('scripts', /main-[a-z0-9]{10}\.min\.js/);
     done();
   });
 
@@ -33,3 +34,18 @@ describe('production', function () {
     done();
   });
 });
+
+/**
+ * Checks if the file of the passed type is minified.
+ *
+ * @param {string} type
+ *   The type of the passed file. Use 'styles' for CSS and 'scripts' for JS.
+ * @param {object} regex
+ *    The regular expression to test if the file is minified.
+ */
+function checkMinification(type, regex) {
+  glob(`test-website/dist/${type}/main*`, function (er, filename) {
+    expect(regex.test(filename)).to.be.true;
+    expect(filename[0]).to.have.contents.that.match(/\*\/(?!\n).*$/);
+  });
+}
